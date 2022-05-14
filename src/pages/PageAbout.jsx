3988fed8877c1831ea4d68f76a3ce7a745f48357
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-14 07:00:32 +0200

    tabs

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -1,9 +1,13 @@
-export const PageAbout = ({books, members}) => {
+export const PageAbout = ({ books, members }) => {
 	return (
 		<div className="page_about">
 			<h2>About</h2>
 			<p>We have been a club for 5 years now.</p>
-			<p>We would love to have you as member #{members.length + 1} and hear your suggestion for book #{books.length + 1}, please write to us if interested.</p>
+			<p>
+				We would love to have you as member #{members.length + 1} and
+				hear your suggestion for book #{books.length + 1}, please write
+				to us if interested.
+			</p>
 		</div>
-	)
-}
+	);
+};
